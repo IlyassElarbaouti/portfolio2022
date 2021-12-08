d964ed699478d65e927241e12be3b0c11aecbed8
Ilyass El Arbaouti <mrfixer59@gmail.com>
2021-12-08 04:24:41 +0200

    fixed loader width

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -4,7 +4,6 @@ const Loader = () => {
     const techs = ['REACT','REDUX','JAVASCRIPT','NEXT.JS','SASS','HAVE FUN! 🤓'];
     return (
       <div className="loader">
-          <div className="test"></div>
           <div className="loader__container">
             <span className="loader__logo">💻</span>
             <ul className="loader__techs">
